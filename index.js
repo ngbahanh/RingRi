77dc0293755f66853f2c7095c5ec4f66e502fed7
Ba Hanh <bahanh268@gmail.com>
2023-07-27 14:38:56 +0700

    config(ios): remove Flipper when run iOS

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+const modules = require.getModules();
+const moduleIds = Object.keys(modules);
+
+const loadedModuleNames = moduleIds
+  .filter(moduleId => modules[moduleId].isInitialized)
+  .map(moduleId => modules[moduleId].verboseName);
+const waitingModuleNames = moduleIds
+  .filter(moduleId => !modules[moduleId].isInitialized)
+  .map(moduleId => modules[moduleId].verboseName);
+
+console.log(
+  'loaded:',
+  loadedModuleNames.length,
+  'waiting:',
+  waitingModuleNames.length,
+);
+
+console.log(
+  `module.exports = ${JSON.stringify(loadedModuleNames.sort(), null, 2)};`,
+);
+
 AppRegistry.registerComponent(appName, () => App);
