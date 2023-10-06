6d5add6e31268d76071e7e8337f5b3c7205453ff
Ba Hanh <toilabahanh@gmail.com>
2023-10-07 04:32:48 +0700

    config: update babel for alias import

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,25 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@configs': './src/configs',
+          '@hooks': './src/hooks',
+          '@locales': './src/locales',
+          '@navigation': './src/navigation',
+          '@redux': './src/redux',
+          '@screens': './src/screens',
+          '@themes': './src/themes',
+        },
+      },
+    ],
+  ],
 };
